b42f8c66e743b7245f0214c7f0968a6c5e41f9ce
Narayanprajapat <narayanprajapat00099@gmail.com>
2022-07-29 01:38:23 +0530

    Course added into enrollment

--- app/services/students.py
+++ app/services/students.py
@@ -1,7 +1,10 @@
+from bson import ObjectId
+
 from app.helper.response_maker import response_maker
 from app.helper import match_value
 from app.models.students import Students
 from app.models.courses import Courses
+from app.models.enrollment import Enrollments
 from datetime import datetime
 
 
@@ -104,15 +107,43 @@ def show_courses_api():
 
 def add_courses_enrollment_api(request):
     try:
-        print(request.form)
+        req = request.get_json()
+        email = req['email']
+        course_id = req['course_id']
     except Exception as e:
         print(e)
         return response_maker({"message": "Invalid input"}, 400)
 
     try:
-        resp = Students().create({"name": 1})
-        print(resp)
-        return response_maker({"message": "Success"}, 201)
+
+        if not match_value(email, r"^\S+@\S+\.\S+$"):
+            return response_maker({"message": "Invalid instructor email"}, 400)
+
+        if len(course_id) is not 24:
+            return response_maker({"message": "Invalid course id, length is 24"}, 400)
+
+        student_resp = Students().read_one({"email": email}, {"_id": 1})
+        if student_resp is None:
+            return response_maker({"message": "User not exist"}, 400)
+
+        course_resp = Courses().read_one({"_id": ObjectId(course_id)}, {"_id": 1, "title": 1})
+        if course_resp is None:
+            return response_maker({"message": "Course id not exist"}, 400)
+
+        payload = {
+            "student_id": str(student_resp['_id']),
+            "course_id": course_id,
+            'created_at': datetime.now(),
+            'updated_at': datetime.now(),
+        }
+
+        query = {"student_id": str(student_resp['_id']), "course_id": course_id}
+        enrollment_resp = Enrollments().read_one(query, query)
+        print(enrollment_resp)
+        if enrollment_resp is None:
+            Enrollments().create(payload)
+            return response_maker({"message": "Successfully course added", "course_name": course_resp['title']}, 200)
+        return response_maker({"message": "Course already added"}, 400)
     except Exception as e:
         print(e)
         return response_maker({"message": "Internal server error"}, 500)

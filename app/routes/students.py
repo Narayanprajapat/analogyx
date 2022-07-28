f8d826197d246e61153d03fb75739764e107ebe7
Narayanprajapat <narayanprajapat00099@gmail.com>
2022-07-29 01:03:00 +0530

    Chnage students methods GET -> POST

--- app/routes/students.py
+++ app/routes/students.py
@@ -9,11 +9,11 @@ def create_student():
     return create_student_api(request)
 
 
-@student.route('/show-courses', methods=['GET'])
+@student.route('/show-courses', methods=['POST'])
 def show_courses():
     return show_courses_api(request)
 
 
-@student.route('/add-courses-enrollment', methods=['GET'])
+@student.route('/add-courses-enrollment', methods=['POST'])
 def add_courses_enrollment():
     return add_courses_enrollment_api(request)

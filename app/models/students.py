64b747e36d0868f81f8933e5a6a33482f12ec100
Narayanprajapat <narayanprajapat00099@gmail.com>
2022-07-28 21:42:23 +0530

    Define students database model and query

--- app/models/students.py
+++ app/models/students.py
@@ -1,3 +1,22 @@
 from app.models import database
 
-print(database)
+
+class Students:
+    def __init__(self):
+        self._filter = None
+        self.query = None
+        self.data = None
+
+    def create(self, data):
+        self.data = data
+        return database.students.insert_one(self.data)
+
+    def read(self, _filter, query):
+        self._filter = _filter
+        self.query = query
+        return database.students.find(self._filter, self.query)
+
+    def read_one(self, _filter, query):
+        self._filter = _filter
+        self.query = query
+        return database.students.find_one(self._filter, self.query)

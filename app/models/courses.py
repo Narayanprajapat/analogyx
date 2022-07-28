b60fcfb27c7e6cdde89b8bb549f434f46b79086d
Narayanprajapat <narayanprajapat00099@gmail.com>
2022-07-28 21:20:08 +0530

    Define course database model and query

---
+++ app/models/courses.py
@@ -0,0 +1,22 @@
+from app.models import database
+
+
+class Course:
+    def __init__(self):
+        self._filter = None
+        self.query = None
+        self.data = None
+
+    def create(self, data):
+        self.data = data
+        return database.courses.insert_one(self.data)
+
+    def read(self, _filter, query):
+        self._filter = _filter
+        self.query = query
+        return database.courses.find(self._filter, self.query)
+
+    def read_one(self, _filter, query):
+        self._filter = _filter
+        self.query = query
+        return database.courses.find_one(self._filter, self.query)
--- app/services/courses.py
+++ app/services/courses.py
@@ -1,16 +1,19 @@
 from app.helper.response_maker import response_maker
 from app.helper import match_value
+from app.models.courses import Course
 
 
 def create_course_api(request):
-    try:
-        print(request.form)
-    except Exception as e:
-        print(e)
-        return response_maker({"message": "Invalid input"}, 400)
+    # try:
+    #     print(request.form)
+    # except Exception as e:
+    #     print(e)
+    #     return response_maker({"message": "Invalid input"}, 400)
 
     try:
-        return response_maker({}, 201)
+        print("Create")
+        Course().create({"name": "Narayan"})
+        return response_maker({"message": "Success"}, 201)
     except Exception as e:
         print(e)
         return response_maker({"message": "Internal server error"}, 500)
